b4de23eaa79643f193149dce484f6f80ccdbf90f
Zhengyi Li <lizhengyi@pku.edu.cn>
2021-10-03 14:46:50 +0800

    Update and rename generate.bash to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,9 @@
 # for vercel
-wget https://github.com/jgm/pandoc/releases/download/2.9/pandoc-2.9-1-amd64.deb
-sudo dpkg -i ./pandoc-2.9-1-amd64.deb
+yum install wget
+mkdir pandoc
+wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | \
+   tar xvzf - --strip-components 1 -C ./pandoc
+export PATH="./pandoc/bin:$PATH"
 
 sed -i 's#<img src="\.\./images/\(.*\?\)/>#<img src="/images/\1/>#g' ./source/_posts/*.md
 sed -i -r 's#^!\[\.\./images/(.*)]\((.*)\)#<img src="/images/\2" alt="\1" style="zoom:100%;" />#g' ./source/_posts/*.md
